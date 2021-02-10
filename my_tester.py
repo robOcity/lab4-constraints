d2fe249d883b9591d09d3c502040a650f2018a3e
Rob Osterburg <robert.osterburg@gmail.com>
2021-02-09 21:42:29 -0700

    adding a manual test

--- my_tester.py
+++ my_tester.py
@@ -11,3 +11,5 @@ solve_constraint_dfs_2_expected = (
 
 result = solve_constraint_dfs(get_pokemon_problem())
 print(result)
+
+# Hint: Look at search.py, specifically the Edge and UndirectedGraph classes.  Path are lists of vertices connected by edges.  Edges have length.  Graphs have a get_edge method.  Iterate over the edges in the path and add up the lengths.  Hope this helps!
